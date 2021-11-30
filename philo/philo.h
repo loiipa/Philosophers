d1e6ebec7c7e54f88bec91e4a885d8fdd14f079e
Jang Chang Hyun <cjang@student.42seoul.kr>
2021-11-30 20:12:23 +0900

    just test2_쓰레드와 뮤텍스에 대한 사용법을 알기 위해 코드 시험

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 18:21:08 by cjang             #+#    #+#             */
-/*   Updated: 2021/11/25 20:33:35 by cjang            ###   ########.fr       */
+/*   Updated: 2021/11/26 20:40:54 by cjang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@
 
 typedef struct s_philo
 {
-	unsigned int	num_of_philo;
-	unsigned int	time_to_die;
-	unsigned int	time_to_eat;
-	unsigned int	time_to_sleep;
-	unsigned int	limit_num_of_eat;
+	unsigned int		num_of_philo;
+	unsigned int		time_to_die;
+	unsigned int		time_to_eat;
+	unsigned int		time_to_sleep;
+	unsigned int		limit_num_of_eat;
 }t_philo;
 
 int	ft_atoi(const char *str);
